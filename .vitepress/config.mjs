43e86725e79f9515c49093ab3fbcc5d420b31a92
Shahjahan Jewel <1053500+techjewel@users.noreply.github.com>
2025-06-12 17:29:36 +0600

    Update config.mjs

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -188,9 +188,9 @@ export default {
       ]
     },
     logo: {
-      dark: './public/logo-full.svg',
-      light: './public/logo-full-dark.svg',
+      dark: './logo-full.svg',
+      light: './logo-full-dark.svg',
     },
   },
-  head: [['link', { rel: 'icon', href: './public/icon.webp' }]]
+  head: [['link', { rel: 'icon', href: './icon.webp' }]]
 }
