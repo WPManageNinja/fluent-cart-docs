d0dcb545734fc08fb1a1256fd2ac6fce0ab1bd15
Md Kamrul Islam <kevin@wpmanageninja.com>
2025-07-03 17:15:35 +0600

    Revert "added image-popup support"
    This reverts commit 3a3d54955ea1f0341319b7a722d0ffa0fa906453.

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,11 +1,9 @@
 import DefaultTheme from 'vitepress/theme'
 import Breadcrumbs from './components/Breadcrumbs.vue'
-import ZoomableImage from './components/ZoomableImage.vue'
 
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     app.component('Breadcrumbs', Breadcrumbs)
-    app.component('ZoomableImage', ZoomableImage)
   }
 } 
