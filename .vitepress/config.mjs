719303f110a16262bccf73b8c86eff110d9a101e
ah-rasel5 <127735115+ah-rasel5@users.noreply.github.com>
2025-09-25 17:15:00 +0600

    Added Order Bump Doc
    Added Order Bump Documentation

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -226,6 +226,7 @@ export default defineConfig({
                 { text: 'Editing Existing Orders', link: '/guide/store-management/orders-management/editing-existing-orders' },
                 { text: 'Processing Refunds', link: '/guide/store-management/orders-management/processing-refunds' },
                 { text: 'Collecting Payments for Modified Orders', link: '/guide/store-management/orders-management/collecting-payments-modified-orders' },
+                { text: 'Order Bump', link: '/guide/store-management/orders-management/order-bump' },
                 { text: 'Changing Order Statuses', link: '/guide/store-management/orders-management/changing-order-statuses' },
                 { text: 'Understanding Statuses', link: '/guide/store-management/understanding-statuses' }
               ]
