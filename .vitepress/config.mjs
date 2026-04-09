a4ec05f15c693ea900318b504e304c321d18518c
ah-rasel5 <127735115+ah-rasel5@users.noreply.github.com>
2026-04-09 11:35:02 +0600

    Image Zoom Feature added

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vitepress'
 import { joinURL, withoutTrailingSlash } from 'ufo'
-// import { zoomablePlugin } from './theme/markdown-plugin-zoomable'
+import { zoomablePlugin } from './theme/plugin-zoomable.ts'
 
 export default defineConfig({
   title: 'FluentCart Documentation',
@@ -141,8 +141,9 @@ export default defineConfig({
   },
   
   markdown: {
+    lineNumbers: true,
     config: (md) => {
-      // md.use(zoomablePlugin) // Disabled - ZoomableImage component causing frontend rendering issues
+      md.use(zoomablePlugin)
       // Open all links (internal and external) in a new tab
       const defaultLinkOpenRenderer = md.renderer.rules.link_open || ((tokens, idx, options, env, self) => self.renderToken(tokens, idx, options))
       md.renderer.rules.link_open = (tokens, idx, options, env, self) => {
@@ -162,7 +163,7 @@ export default defineConfig({
         }
         return defaultLinkOpenRenderer(tokens, idx, options, env, self)
       }
-    }
+    },
   },
   vite: {
     publicDir: 'guide/public',
